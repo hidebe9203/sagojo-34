975cd07d70b1b18f5f7ffe5b1372420ac2aff94f
比嘉将吾 <handball.7@i.softbak.jp>
2018-09-21 14:39:13 +0900

    指摘点の修正

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -1,3 +1,18 @@
+// $(document).ready(function(){
+//   $('a[href^="#"]').click(function() {
+//     var href= $(this).attr("href");
+//     var hash = href == "#" || href == "" ? 'html' : href;
+//     scrollToAnker(hash);
+//     return false;
+//   });
+
+//   function scrollToAnker(hash) {
+//     var target = $(hash);
+//     var position = target.offset().top;
+//     $('body,html').animate({scrollTop:position}, 500);
+//   }
+// })
+
 $(document).ready(function(){
   $('a[href^="#"]').click(function() {
     var href= $(this).attr("href");
