76049e8b07caf3d398763d6717a9a744d95e39f0
比嘉将吾 <handball.7@i.softbak.jp>
2018-09-21 12:45:20 +0900

    指摘点の修正

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -1,33 +1,14 @@
 $(document).ready(function(){
-  //URLのハッシュ値を取得
-  var urlHash = location.hash;
-  //ハッシュ値があればページ内スクロール
-  if(urlHash) {
-    //スクロールを0に戻しておく
-    $('body,html').stop().scrollTop(0);
-    setTimeout(function () {
-      //ロード時の処理を待ち、時間差でスクロール実行
-      scrollToAnker(urlHash) ;
-    }, 100);
-  }
-
-  //通常のクリック時
   $('a[href^="#"]').click(function() {
-    //ページ内リンク先を取得
     var href= $(this).attr("href");
-    //リンク先が#か空だったらhtmlに
     var hash = href == "#" || href == "" ? 'html' : href;
-    //スクロール実行
     scrollToAnker(hash);
     return false;
   });
 
-  // 関数：スムーススクロール
-  // 指定したアンカー(#ID)へアニメーションでスクロール
   function scrollToAnker(hash) {
     var target = $(hash);
     var position = target.offset().top;
-    console.log('position');
-    $('body,html').stop().animate({scrollTop:position}, 500);
+    $('body,html').animate({scrollTop:position}, 500);
   }
 })
