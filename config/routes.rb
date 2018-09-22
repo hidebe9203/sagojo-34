80f369c4a783598b093a9100da2403837bf66291
比嘉将吾 <handball.7@i.softbak.jp>
2018-09-22 15:30:08 +0900

    Add company-sign_up2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+devise_for :companies, controllers: {
+  sessions:      'companies/sessions',
+  passwords:     'companies/passwords',
+  registrations: 'companies/registrations'
+}
+devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
   root to: "tops#index"
   resources :tops, only: [:index]
   resources :works
