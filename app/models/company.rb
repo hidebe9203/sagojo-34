809fc09228a7aa9f9683b190e63418931e7a7466
yuki hosokawa <umaumachuchu@gmail.com>
2018-09-24 11:25:29 +0900

    edit model
    アソシエーションを設定。occupationモデルは未実装のため、アソシエーション部分を一時的にコメントアウトしました。

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,5 @@ class Company < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :works
 end
