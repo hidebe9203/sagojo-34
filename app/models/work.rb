6b9648eafcaf859be7e298f386734ff75eaa6ffc
yuki hosokawa <umaumachuchu@gmail.com>
2018-09-26 14:18:50 +0900

    add methods
    showメソッドを追加。
    募集締切までの残り日数を算出するメソッドをモデルに追加。

--- app/models/work.rb
+++ app/models/work.rb
@@ -8,4 +8,16 @@ class Work < ApplicationRecord
 
   validates :title, :content, :image, :detail, :duration, :requierment, :member, :limit, presence: true
   mount_uploader :image, ImageUploader
+
+
+  # シゴト募集締切までの残り日数を算出
+  require "date"
+
+  def self.days_left(work)
+    limit_days = work.limit
+    today = Date.today
+    days_left = limit_days - today
+    return days_left.to_i
+  end
+
 end
