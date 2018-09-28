e6c19b596c3257ec733579fc8e7220724f7d8cd5
hidebe9203 <kskhidebe9203@gmail.com>
2018-09-28 16:43:03 +0900

    fixed routint, controller, views

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,11 @@
+class ProfilesController < ApplicationController
+  def edit
+    @profile = Profile.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,2 @@
 class UsersController < ApplicationController
-  def edit
-    @profile = Profile.new
-  end
-
-  def create
-  end
-
-  def update
-    binding.pry
-  end
 end
