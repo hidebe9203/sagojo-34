b90b18c12cd22ab87d2d1e3b5b736739107c39dd
yuki hosokawa <umaumachuchu@gmail.com>
2018-09-29 15:17:50 +0900

    コントローラ編集
    indexアクションを定義

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,13 @@
 class TopsController < ApplicationController
+
   def index
+    @works = Work.page(params[:page]).per(6).order("created_at DESC")
+
+    # スライドショーに表示するシゴト
+    @slides = Work.limit(3).order('created_at DESC')
+
+    @works.each do |work|
+      @days_left = Work.days_left(work)
+    end
   end
 end
