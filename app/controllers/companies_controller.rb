d477f12674fc9eb412d0c90d0d3e11b2469dd9ef
yuki hosokawa <umaumachuchu@gmail.com>
2018-09-30 21:22:09 +0900

    ルートとコントローラ編集

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,7 @@
+class CompaniesController < ApplicationController
+
+  def show
+    @company = Company.find(params[:id])
+    @works = @company.works.page(params[:page]).per(5).order("created_at DESC")
+  end
+end
