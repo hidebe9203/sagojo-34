a21b0391839104a4c4c563a5c1baf5e8f917d7d2
hidebe9203 <kskhidebe9203@gmail.com>
2018-10-01 15:39:22 +0900

    Delete comment-out and apply.js
    ・コメントアウトを削除
    ・apply.jsが不必要となったため、削除

--- app/assets/javascripts/apply.js
+++
@@ -1,7 +0,0 @@
-$(window).on('load', function() {
-  if(document.URL.match(/works\/\d+\/forms\/new/)) {
-    postId = $('#applyId').val();
-
-    $('.aplication-area-03.profile-form').append('<input type="submit" name="commit" value="応募する" class="btn w-100" id="prof-update" data-disable-with="応募する">');
-  }
-})
