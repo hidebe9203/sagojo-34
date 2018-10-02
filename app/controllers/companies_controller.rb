d9db88af160bfb334595c09033fc842a6ae14657
hidebe9203 <kskhidebe9203@gmail.com>
2018-10-02 21:07:05 +0900

    Create view apply menber
    ・企業が案件応募者を確認できるビューを作成

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,6 +2,6 @@ class CompaniesController < ApplicationController
 
   def show
     @company = Company.find(params[:id])
-    @works = @company.works.page(params[:page]).per(5).order("created_at DESC")
+    @works = @company.works.page(params[:page]).per(5).order("created_at DESC").includes(:workstatuses)
   end
 end
