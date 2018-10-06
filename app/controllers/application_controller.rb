cddcdcfc1c145ea4cdb6cdb0ff3f8fa4c3f6442f
hidebe9203 <kskhidebe9203@gmail.com>
2018-10-06 16:52:41 +0900

    Delete describe basic certification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
 
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :company_name, :number])
-  end
-  
-# 本番環境でBasic認証を行う
-  def production?
-    Rails.env.production?
-  end
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
   end
 
 end
